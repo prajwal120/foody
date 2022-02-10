9574cd1833e11262674bdc55fdccf1c01ec44749
prajwal120 <myherodad23@gmail.com>
2022-02-10 16:21:25 +0530

    3rd commit for Modules and Routes

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const express = require('express');
 const app =express();
 const mongoose = require('mongoose');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const userroute =require('./routes/user')
+const authroute =require('./routes/auth')
 dotenv.config(); 
 //establishing the authotication with the server i.e... mongodb
 mongoose.connect(
@@ -10,9 +12,11 @@ mongoose.connect(
     )//giving a then and catch log whether to say the connection established or not....
     .then(() =>console.log("DBConnection succesfull")).catch((err) => {console.log(err)});
     //establishing a api to alert the user whether the commit made is successfull or not...
-    app.get("/api/test",()=>{
-        console.log("test is successful")    
-    });
+
+app.use(express.json());
+app.use("/api/auth",authroute);
+app.use("/api/users",userroute);
+
 app.listen(process.env.PORT || 5000, ()=>{
     console.log('Backend server is running');
-});
+}); 
---
+++ routes/user.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();//Router function..
+
+
+module.exports = router;
